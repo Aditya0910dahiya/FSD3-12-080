958de1c23c98a493356914db0cf155ebe0abcf89
Aditya Dahiya <chaudharyadityadahiya@gmail.com>
2026-07-23 11:53:56 +0530

    initial commit

---
+++ lab1/hello.js
@@ -0,0 +1,13 @@
+console.log("Aditya Dahiya");
+
+function sun(a, b) {
+    return a + b;
+}
+
+const add = (a, b) => {
+    return a + b;
+};
+const add2 = (a, b) => a + b;
+console.log(sun(20, 10));
+console.log(add(20, 10));
+console.log(add2(20, 10));
